eb630a3fb3e204483cabed072eb8b46187ad2cb1
Belly0919 <110910819+Belly0919@users.noreply.github.com>
2026-01-15 01:58:03 +0800

    Update IpCard.vue

--- src/components/IpCard.vue
+++ src/components/IpCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ip-card">
-    <img src="https://ipinfo.cv/api/ip/card.png?template=compact" alt="IP信息卡" />
+    <img src="https://ipinfo.cv/api/ip/card.png?template=default" alt="IP信息卡" />
   </div>
 </template>
 
