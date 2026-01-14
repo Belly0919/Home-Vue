d0d44a599ecc35b60d8760503bc0b3146342184d
Belly0919 <110910819+Belly0919@users.noreply.github.com>
2026-01-15 01:37:06 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="background"></div>
-    <Home />
+
+  <Home />
+
+  <!-- IP 信息卡 -->
+  <div class="ip-card">
+    <img src="https://ipinfo.cv/api/ip/card.png?template=default" alt="IP信息卡" />
+  </div>
+
   <footer>
     <span>© 2025 Made in <a href="/" target="_blank">{{ userName }}</a></span>
     <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
-    <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
+    <a v-if="policenumber"
+       :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`"
+       target="_blank"
+       class="police_link">
       <span class="police_img"></span> {{ policenumber }}
     </a>
   </footer>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-import Home from './components/Home.vue';
-
-const userName = ref(import.meta.env.VITE_APP_USER_NAME);
-const icpNumber = ref(import.meta.env.VITE_APP_ICP_NUMBER);
-const policenumber = ref(import.meta.env.VITE_APP_POLICE_NUMBER);
-</script>
